4ff99134bf5b40ad5cb8b8a7c103f801bd70969c
Tarik Crnovrsanin <turokhunter@gmail.com>
2020-07-15 19:49:59 -0700

    Write project setup.

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,7 +1,63 @@
 import React from 'react';
 import Layout from '../../components/Layout';
-import { Row} from 'react-bootstrap'
+import {Figure, Row, Col} from 'react-bootstrap'
 // import { ProfileLink } from './styles';
+
+
+const Html = ({ele}) => {
+  if(ele.p){
+    return (
+      <Col xs={18} md={12}>
+        <p>{ele.p}</p>
+      </Col>
+    )
+  } else if(ele.image){
+    var image = ele.image;
+    return (
+      <Col xs={12} md={8}>
+        <Figure>
+          <Figure.Image 
+          height = {image.height ? image.height : "100%"}
+          width = {image.width ? image.width : "100%"}
+          src = {image.src}
+          />
+          <Figure.Caption>
+            {image.caption}
+          </Figure.Caption>
+        </Figure>
+      </Col>
+    )
+  } else if(ele.video){
+    var video = ele.video;
+    return (
+      <Col xs={9} md={4}>
+        <iframe 
+          width = {video.width ? video.width : "100%"}
+          height = {video.height ? video.height : "100%"}
+          src = {video.src}
+          frameBorder= "0"
+          allow = {video.allow}
+          allowFullScreen>
+
+          </iframe>
+        
+      </Col>
+    )
+  } else if(ele && ele.length > 1){
+    return (
+      <>
+      { ele.map(entry =>(
+          <Html ele={entry}/>              
+      ))}
+      </>
+    )
+  } else {
+    return (
+      <p>else</p>
+    )
+  }
+}
+
 
 const Entry = ({entry}) => {
   return (
@@ -11,9 +67,14 @@ const Entry = ({entry}) => {
           {entry.title}
         </h5>
       </Row>
-      <div dangerouslySetInnerHTML = {{__html:entry.html}}></div>
+      { entry.html.map(html =>(
+        <Row className="justify-content-md-center">
+          <Html ele={html}/>              
+        </Row>
+      ))}
+      {/* <div dangerouslySetInnerHTML = {{__html:entry.html}}></div> */}
     </>
-  )
+  );
 }
 
 
